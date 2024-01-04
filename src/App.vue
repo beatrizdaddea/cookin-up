9018865e0cd18c9da14f2a40c77c80fd34774ea4
beatrizdaddea <beatrizchith@gmail.com>
2024-01-04 14:23:33 -0300

    wip: main content

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <Banner />
+  <MainContent />
 </template>
 
 <script lang="ts">
 
 import Banner from './components/Banner.vue';
+import MainContent from './components/MainContent.vue';
 
 export default {
-  components: { Banner }
+  components: { Banner, MainContent }
 }
 
 </script>
+
+<style scoped>
+  
+</style>
