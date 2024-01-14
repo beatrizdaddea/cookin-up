5dea5e9f374d76f86d1175aa2cee3fa28b52fbae
beatrizdaddea <beatrizchith@gmail.com>
2024-01-13 21:32:03 -0300

    fix: your list style bug

--- src/components/YourList.vue
+++ src/components/YourList.vue
@@ -18,7 +18,7 @@ export default {
 
     <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
       <li v-for="ingrediente in ingredientes" :key="ingrediente">
-        <Tag :texto="ingrediente" ativa />
+        <Tag :texto="ingrediente" active />
       </li>
     </ul>
 
